0b0efa54b959b74875824a5cd6e55f3c74b27db4
Dominggoes Isakh <drjisakh@gmail.com>
2020-08-29 08:52:34 +0200

    exynos4: hwc: Use libfimg4x for 4210
    Change-Id: I5ee41e12bbc2dd15991e7ac7b1bf1897142a9911

--- exynos4/hal/libhwc/Android.mk
+++ exynos4/hal/libhwc/Android.mk
@@ -26,10 +26,6 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libhardware libutils libsync libfimg
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),smdk4210)
-    LOCAL_CFLAGS += -DNO_FIMG
-endif
-
 LOCAL_SRC_FILES := hwcomposer.cpp \
                    hwcomposer_vsync.cpp \
                    window.cpp \
